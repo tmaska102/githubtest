9008d7cba22ec30fdc9e643aba62eb3bc1618610
tmaska102 <74671938+tmaska102@users.noreply.github.com>
2020-12-10 14:04:02 -0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+//document.addEventListener('DOMContentLoaded', function(){
+   
+  //  var highFiveButton = document.getElementById('highFive');
+  //  highFiveButton.addEventListener('click', function(){
+   //     onclick="this.ParentElement.style.display='none';"
+   // })
+
+//})
